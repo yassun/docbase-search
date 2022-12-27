5500d7d394fbdb18e63e5a35e08ca683a550ec19
yassun <yasukun.taga@gmail.com>
2022-12-27 12:30:02 +0900

    add teamname to prefarence / fix lint

--- src/docbase-search.tsx
+++ src/docbase-search.tsx
@@ -1,42 +1,46 @@
-import { List, Action, ActionPanel, getPreferenceValues, showToast, Toast } from "@raycast/api"
-import { useState, useEffect } from "react"
-import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios"
+import { List, Action, ActionPanel, getPreferenceValues, showToast, Toast } from "@raycast/api";
+import { useState, useEffect } from "react";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 interface Preferences {
-  accessToken: string
+  accessToken: string;
+  teamName: string;
 }
 
 type Props = {
   arguments: {
-    keyword: string
-  }
-}
+    keyword: string;
+  };
+};
 
-// ref https://help.docbase.io/posts/45703
-type PostItem = { id: number, title: string, body: string, url: string }
+// ref. https://help.docbase.io/posts/45703
+type PostItem = { id: number; title: string; body: string; url: string };
 
 export default function Command(props: Props) {
   const preferences = getPreferenceValues<Preferences>();
-  const [searchKeyword, setSearchKeyword] = useState(props.arguments.keyword || "")
-  const [posts, setPosts] = useState<PostItem[]>([])
-  const resultInfo = searchKeyword ? ( posts ? `${posts?.length} posts` : `searching for ${searchKeyword}`)  : "search for posts...";
+  const [searchKeyword, setSearchKeyword] = useState(props.arguments.keyword || "");
+  const [posts, setPosts] = useState<PostItem[]>([]);
+  const resultInfo = searchKeyword
+    ? posts
+      ? `${posts?.length} posts`
+      : `searching for ${searchKeyword}`
+    : "search for posts...";
 
   useEffect(() => {
     const options: AxiosRequestConfig = {
-      url: `https://api.docbase.io/teams/lecto/posts?q=${searchKeyword}`,
+      url: `https://api.docbase.io/teams/${preferences.teamName}/posts?q=${searchKeyword}`,
       method: "GET",
-      headers: { 'X-DocBaseToken': preferences.accessToken }
-    }
+      headers: { "X-DocBaseToken": preferences.accessToken },
+    };
 
     axios(options)
       .then((res: AxiosResponse) => {
-        const { data } = res
-        setPosts(data.posts)
+        const { data } = res;
+        setPosts(data.posts);
       })
       .catch((e: AxiosError<{ error: string }>) => {
-        showToast(Toast.Style.Failure, e.message)
-      })
-
+        showToast(Toast.Style.Failure, e.message);
+      });
   }, [searchKeyword]);
 
   return (
